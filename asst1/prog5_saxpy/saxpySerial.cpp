e07f36502cac839ce21a9683f735710e3aed8073
kris <kristonwang02@gmail.com>
2024-12-27 16:04:38 -0500

    Found that the non-speedup is due to memory bandwidth--loads 4x32 bits every time for one computation. Computation-memory ration is too low. Achieving a greater speedup of 15.09 from ISPC  by adding unecessary computation that increases computation-memory ration. Finished lab1

--- asst1/prog5_saxpy/saxpySerial.cpp
+++ asst1/prog5_saxpy/saxpySerial.cpp
@@ -7,7 +7,12 @@ void saxpySerial(int N,
 {
 
     for (int i=0; i<N; i++) {
-        result[i] = scale * X[i] + Y[i];
+        float temp = 1.f;
+        for (int j = 0; j < 100; j++) {
+            temp = temp * 1.0001f + 0.0001f; // Arbitrary computation to increase load
+        }
+        result[i] = scale * X[i] + Y[i] + temp - temp;
+        
     }
 }
 
